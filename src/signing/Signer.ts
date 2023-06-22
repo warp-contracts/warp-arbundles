2b6527e9362a3676443bab73e401a875a37bd8b1
Asia <ziola.jm@gmail.com>
2023-06-22 14:50:13 +0200

    setPublicKey method

--- src/signing/Signer.ts
+++ src/signing/Signer.ts
@@ -11,6 +11,7 @@ export abstract class Signer {
 
   abstract sign(message: Uint8Array, _opts?: any): Promise<Uint8Array> | Uint8Array;
   abstract signDataItem?(dataItem: string | Buffer, tags: Tag[]): Promise<DataItem>;
+  abstract setPublicKey?(): Promise<void>;
   static verify(_pk: string | Buffer, _message: Uint8Array, _signature: Uint8Array, _opts?: any): boolean {
     throw new Error('You must implement verify method on child');
   }

b15c0ca88ed11c180f8478a7dd4219d4f42604e4
Asia <ziola.jm@gmail.com>
2023-06-27 15:28:37 +0200

    feat: add getAddress method to Signer

--- src/signing/Signer.ts
+++ src/signing/Signer.ts
@@ -12,6 +12,7 @@ export abstract class Signer {
   abstract sign(message: Uint8Array, _opts?: any): Promise<Uint8Array> | Uint8Array;
   abstract signDataItem?(dataItem: string | Buffer, tags: Tag[]): Promise<DataItem>;
   abstract setPublicKey?(): Promise<void>;
+  abstract getAddress?(): Promise<string>;
   static verify(_pk: string | Buffer, _message: Uint8Array, _signature: Uint8Array, _opts?: any): boolean {
     throw new Error('You must implement verify method on child');
   }
